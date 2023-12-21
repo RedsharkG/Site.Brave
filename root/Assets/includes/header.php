eb81e466bf96e0b7c6c548bc408b5608f5d9cc78
Shehara Gishan <141002617+RedsharkG@users.noreply.github.com>
2023-12-22 00:30:09 +0530

    Header
    Finalising

--- root/Assets/includes/header.php
+++ root/Assets/includes/header.php
@@ -1,1 +1,46 @@
 <?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../styles/headerStyle.css" rel="stylesheet"/>
+    <script type = "text/javascript" src = "../scripts/headerScript.js"></script>
+</head>
+<body>
+
+<header>
+<div id = "header-container">
+<ul id = "header-nav">
+        <li><a href = "#Home" class = "active"> Home </a> </li>
+
+    <li>
+        <a href = "#services"> Services </a>
+        <ul class = "dropdown">
+            <li><a href="#Sports"> Sports </a> </li>
+            <li> <a href = "#casuals"> Casuals </a></li>
+            <li> <a href = "#Winter"> Winter </a></li>
+        </ul>
+    </li>
+
+    <li> <a href = "#Our Team"> About Us </a></li>
+
+    <li>
+        <a href = "#Login"> Login </a>
+        <ul class = "dropdown">
+            <li><a href="#register">Register</a></li>
+        </ul>
+
+    </li>
+    </ul>
+
+
+</div>
+</header>
+</body>
+</html>
